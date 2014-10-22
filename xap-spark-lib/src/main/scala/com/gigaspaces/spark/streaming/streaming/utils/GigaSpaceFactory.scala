4d69e8efc91c9114f7e070008232fa8fe7fd7426
Oleksiy Dyagilev <oleksiy_dyagilev@epam.com>
2014-10-22 19:08:11 +0300

    avoid creating several redundant GigaSpaces instances on startup

--- xap-spark-lib/src/main/scala/com/gigaspaces/spark/streaming/streaming/utils/GigaSpaceFactory.scala
+++ xap-spark-lib/src/main/scala/com/gigaspaces/spark/streaming/streaming/utils/GigaSpaceFactory.scala
@@ -3,6 +3,8 @@ package com.gigaspaces.spark.streaming.streaming.utils
 import org.openspaces.core.{GigaSpaceConfigurer, GigaSpace}
 import org.openspaces.core.space.UrlSpaceConfigurer
 import scala.collection.mutable
+import java.util.concurrent.ConcurrentHashMap
+import scala.collection.convert.decorateAsScala._
 
 
 /**
@@ -12,16 +14,24 @@ import scala.collection.mutable
  */
 object GigaSpaceFactory {
 
-  private val gigaSpaces = mutable.Map[String, GigaSpace]()
+  private val gigaSpaces = new ConcurrentHashMap[String, GigaSpace]().asScala
 
   // TODO: add group, locator
   def getOrCreate(spaceUrl: String): GigaSpace = {
-    def createInstance = {
-      val urlSpaceConfigurer = new UrlSpaceConfigurer(spaceUrl)
-      new GigaSpaceConfigurer(urlSpaceConfigurer.space()).gigaSpace()
-    }
+    gigaSpaces.getOrElse(spaceUrl, createInstanceIfRequired(spaceUrl))
+  }
 
-    gigaSpaces.getOrElseUpdate(spaceUrl, createInstance)
+  private def createInstanceIfRequired(spaceUrl: String): GigaSpace = {
+    this.synchronized {
+      gigaSpaces.get(spaceUrl) match {
+        case Some(gs) => gs
+        case None =>
+          val urlSpaceConfigurer = new UrlSpaceConfigurer(spaceUrl)
+          val gs = new GigaSpaceConfigurer(urlSpaceConfigurer.space()).gigaSpace()
+          gigaSpaces.put(spaceUrl, gs)
+          gs
+      }
+    }
   }
 
 }
